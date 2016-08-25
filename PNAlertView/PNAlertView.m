8fd14564470afaa11ebf5c7bd31407b4e79a7ec2
liupanniu <panliuliu@139.com>
2016-08-25 14:33:02 +0800

    Release 1.1.1

--- PNAlertView/PNAlertView.m
+++ PNAlertView/PNAlertView.m
@@ -14,6 +14,7 @@
 #define PNScreenHeight [UIScreen mainScreen].bounds.size.height
 
 @interface PNAlertView ()<UIWebViewDelegate>
+
 /// 遮罩层
 @property(nonatomic, weak) UIView *coverV;
 /// titleLbl
@@ -28,9 +29,14 @@
 @property(nonatomic, weak) UIButton *cancelBtn;
 /// 确定按钮
 @property(nonatomic, weak) UIButton *okBtn;
+
 @end
 
 @implementation PNAlertView
+
+/**
+ 初始化方法
+ */
 - (instancetype)initWithFrame:(CGRect)frame
 {
     if (self = [super initWithFrame:frame]) {
@@ -77,7 +83,7 @@
         
         // 确定按钮
         UIButton *okBtn = [[UIButton alloc] init];
-        [okBtn setBackgroundColor:[UIColor redColor]];
+        [okBtn setBackgroundColor:[UIColor colorWithRed:235/255.0 green:65/255.0 blue:61/255.0 alpha:1.0]];
         [self addSubview:okBtn];
         self.okBtn = okBtn;
         [self.okBtn addTarget:self action:@selector(okBtnClick:) forControlEvents:UIControlEventTouchUpInside];
@@ -86,16 +92,15 @@
     return self;
 }
 
+
+#pragma mark - 提供的外部方法
 - (instancetype)initWithTitle:(NSString *)title content:(NSString *)content cancelBtnTitle:(NSString *)clBtnTitle cancelBlock:(btnBlock)clBlock okBtnTitle:(NSString *)okBtnTitle okBlock:(btnBlock)okBlock
 {
     if (self = [super init]) {
-        
         // 标题
         self.titleLbl.text = title;
-        
         // 内容
         [self.webView loadHTMLString:content baseURL:nil];
-        
         // 按钮标题
         if (clBtnTitle) {
             [self.cancelBtn setTitle:clBtnTitle forState:UIControlStateNormal];
@@ -118,6 +123,8 @@
     }
     return self;
 }
+
+#pragma mark - 显示视图方法
 - (void)show
 {
     UIWindow *keyWindow = [UIApplication sharedApplication].keyWindow;
@@ -148,10 +155,10 @@
         
     });
     
-
-    // 显示的动画效果
-    
 }
+
+
+/// 取消按钮的点击
 - (void)cancelBtnClick:(UIButton *)cancelBtn
 {
     // 视图消失
@@ -167,6 +174,7 @@
         self.cancelBlock();
     }
 }
+/// 确定按钮的点击
 - (void)okBtnClick:(UIButton *)okBtn
 {
     // 视图消失
@@ -184,6 +192,8 @@
     }
 }
 
+
+/// 子视图的布局
 - (void)layoutSubviews
 {
     [super layoutSubviews];
